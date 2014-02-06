de6f96eb418965bad5a520ec814c34c20655831f
inhu <antti_vanhatalo@hotmail.com>
2014-02-06 19:50:14 +0200

    palikat toimii

--- Tetris/src/main/java/tetris/tetris/Main.java
+++ Tetris/src/main/java/tetris/tetris/Main.java
@@ -4,13 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        Ruudukko ruudukko = new Ruudukko(10,20);
+        Ruudukko ruudukko = new Ruudukko(10, 20);
         Kayttoliittyma kayttoliittyma = new Kayttoliittyma(ruudukko);
         Sovelluslogiikka sovelluslogiiikka = new Sovelluslogiikka(ruudukko);
-        ruudukko.tulostaRuudukko();
         Palikka palikka = new Palikka();
-        palikka.tulostaPalikka(palikka.IPalikka());
-        palikka.kaannaPalikka();
-        palikka.tulostaPalikka(palikka.IPalikka());
+        for (int i = 0; i < 10; i++) {
+            palikka.tulostaPalikka(palikka.getPalat());
+            palikka.kaannaPalikka();
+            System.out.println("");
+        }
+        
     }
 }
--- Tetris/src/main/java/tetris/tetris/Palikka.java
+++ Tetris/src/main/java/tetris/tetris/Palikka.java
@@ -15,8 +15,19 @@ public class Palikka {
         int x = r.nextInt(6);
         if (x == 0) {
             return IPalikka();
+        } else if (x == 1) {
+            return JPalikka();
+        } else if (x == 2) {
+            return LPalikka();
+        } else if (x == 3) {
+            return OPalikka();
+        } else if (x==4){
+            return SPalikka();
+        } else if (x==5){
+            return TPalikka();
+        } else {
+            return ZPalikka();
         }
-        return null;
     }
 
     public int[][] IPalikka() {
@@ -24,6 +35,60 @@ public class Palikka {
         for (int i = 0; i < 4; i++) {
             palikka[i][1] = 2;
         }
+        return palikka;
+    }
+
+    public int[][] JPalikka() {
+        int[][] palikka = new int[4][4];
+        palikka[0][1] = 2;
+        palikka[1][1] = 2;
+        palikka[2][1] = 2;
+        palikka[2][2] = 2;
+        return palikka;
+    }
+
+    public int[][] LPalikka() {
+        int[][] palikka = new int[4][4];
+        palikka[0][1] = 2;
+        palikka[1][1] = 2;
+        palikka[2][1] = 2;
+        palikka[0][2] = 2;
+        return palikka;
+    }
+
+    public int[][] OPalikka() {
+        int[][] palikka = new int[4][4];
+        palikka[1][1] = 2;
+        palikka[2][1] = 2;
+        palikka[1][2] = 2;
+        palikka[2][2] = 2;
+        return palikka;
+    }
+
+    public int[][] SPalikka() {
+        int[][] palikka = new int[4][4];
+        palikka[2][1] = 2;
+        palikka[3][1] = 2;
+        palikka[1][2] = 2;
+        palikka[2][2] = 2;
+        return palikka;
+    }
+
+    public int[][] TPalikka() {
+        int[][] palikka = new int[4][4];
+        palikka[1][1] = 2;
+        palikka[0][2] = 2;
+        palikka[1][2] = 2;
+        palikka[2][2] = 2;
+        return palikka;
+    }
+
+    public int[][] ZPalikka() {
+        int[][] palikka = new int[4][4];
+        palikka[1][1] = 2;
+        palikka[2][1] = 2;
+        palikka[2][2] = 2;
+        palikka[3][2] = 2;
         return palikka;
     }
 
@@ -40,9 +105,13 @@ public class Palikka {
         int[][] kaannetty = new int[4][4];
         for (int r = 0; r < 4; r++) {
             for (int c = 0; c < 4; c++) {
-                kaannetty[c][4 - 1 - r] = palat[r][c];
+                kaannetty[c][4 - 1 - r] = this.palat[r][c];
             }
         }
-        tulostaPalikka(kaannetty);
+        this.palat = kaannetty.clone();
+    }
+
+    public int[][] getPalat() {
+        return this.palat;
     }
 }
